ae10d330c03a4ddbd6a271773ff4b679ed256f23
Alessandro Lenzi <alessandro.lenzi@onfido.com>
2021-01-02 19:19:01 +0000

    Added test for SUS

--- evolutionary_programming/selectors/stochastic_universal_sampling.py
+++ evolutionary_programming/selectors/stochastic_universal_sampling.py
@@ -1,5 +1,5 @@
 import random
-from typing import Sequence, Iterable, Tuple, Generic, TypeVar
+from typing import Sequence, Iterable, Tuple, Generic, TypeVar, List
 
 from evolutionary_programming.details import Comparable
 from evolutionary_programming.selectors.selector import IndividualType
@@ -21,24 +21,23 @@ class StochasticUniversalSampling(Generic[IndividualType, ScoreType]):
         self, population: Sequence[Tuple[IndividualType, ScoreType]]
     ) -> Iterable[IndividualType]:
         total_population_fitness_score = sum(float(i[1]) for i in population) or 1
-        pointers_distance = total_population_fitness_score / self.selection_size
-        start = random.uniform(0, pointers_distance)
-        selected_points = [
-            start + pointers_distance * i for i in range(self.selection_size)
-        ]
+        selected_points = self.make_selection_points(total_population_fitness_score)
         total_seen_score: float = 0.0
+
         for individual, score in population:
-            cumulative_mating_probability = float(score) + total_seen_score
+            cumulative_selection_score = float(score) + total_seen_score
             while (
                 selected_points
-                and total_seen_score
-                <= selected_points[0]
-                < cumulative_mating_probability
+                and total_seen_score <= selected_points[0]
+                and selected_points[0] < cumulative_selection_score
             ):
-                selected_points.pop()
+                selected_points.pop(0)
                 yield individual
+            total_seen_score = cumulative_selection_score
 
-            if not selected_points:
-                return
-
-            total_seen_score = cumulative_mating_probability
+    def make_selection_points(
+        self, total_population_fitness_score: float
+    ) -> List[float]:
+        pointers_distance = total_population_fitness_score / self.selection_size
+        start = random.uniform(0, pointers_distance)
+        return [start + pointers_distance * i for i in range(self.selection_size)]
